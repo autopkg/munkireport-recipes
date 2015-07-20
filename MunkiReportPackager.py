fbe86e44e82f4f8743745df9e88e5b58e05abea7
Arjen van Bochoven <arjen@bochoven.net>
2015-07-20 23:52:42 +0200

    Initial commit

---
+++ MunkiReportPackager.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+#
+# Copyright 2010 Per Olofsson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""See docstring for MunkiReportPackager class"""
+
+import os
+import stat
+import subprocess
+import FoundationPlist
+
+from autopkglib import Processor, ProcessorError
+
+
+__all__ = ["MunkiReportPackager"]
+
+
+class MunkiReportPackager(Processor):
+    """Creates a package."""
+    description = __doc__
+    input_variables = {
+        "pathname": {
+            "required": True,
+            "description": "Path to the install script.",
+        },
+        "modules": {
+            "required": False,
+            "description": (
+                "An array of modules to be install."),
+        }
+    }
+    output_variables = {
+        "pkg_path": {
+                "description": "The created package.",
+            }
+    }
+
+    def main(self):
+
+        # Make script executable
+        st = os.stat(self.env["pathname"])
+        os.chmod(self.env["pathname"], st.st_mode | stat.S_IEXEC)
+
+        # Packagedir
+        packagedir = os.path.join(self.env["RECIPE_CACHE_DIR"], "downloads")
+        # Result plist
+        resultplist = os.path.join(self.env["RECIPE_CACHE_DIR"], "result.plist")
+
+        args = [self.env["pathname"], "-i", packagedir, "-r", resultplist]
+
+        # Call script.
+        try:
+            proc = subprocess.Popen(
+                args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            (out, err_out) = proc.communicate()
+        except OSError as err:
+            raise ProcessorError(
+                "script execution failed with error code %d: %s"
+                % (err.errno, err.strerror))
+        if proc.returncode != 0:
+            raise ProcessorError(
+                "creating package for %s failed: %s"
+                % (self.env["pathname"], err_out))
+
+        # Get package path from resultplist
+        result = FoundationPlist.readPlist(resultplist)
+        self.output("Created package %s" % result["pkg_path"])
+        self.env["pkg_path"] = result["pkg_path"]
+
+
+if __name__ == '__main__':
+    PROCESSOR = MunkiReportPackager()
+    PROCESSOR.execute_shell()
---
+++ MunkiReportUrlCreator.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+#
+# Copyright 2015 Arjen van Bochoven
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""See docstring for MunkiReportUrlCreator class"""
+
+from autopkglib import Processor, ProcessorError
+
+__all__ = ["MunkiReportUrlCreator"]
+
+class MunkiReportUrlCreator(Processor):
+    """Provides URL to the latest update."""
+    description = __doc__
+    input_variables = {
+        "baseurl": {
+            "required": True,
+            "description": "URL for a Munkireport packager script.",
+        },
+        "modules": {
+            "required": False,
+            "description":
+                "Array of modules to include in the install.",
+        }
+    }
+    output_variables = {
+        "url": {
+            "description": "URL for a download.",
+        }
+    }
+
+    def main(self):
+        """Get URL for latest version"""
+
+        url = self.env.get("baseurl") + "/index.php?/install/modules"
+
+        # Add modules if provided
+        modules = self.env.get("modules")
+        if modules:
+            for module in modules:
+                url = url + '/' + module
+            # Format description
+
+        self.env["url"] = url
+        self.output("Found URL %s" % self.env["url"])
+
+if __name__ == "__main__":
+    PROCESSOR = MunkiReportUrlCreator()
+    PROCESSOR.execute_shell()
