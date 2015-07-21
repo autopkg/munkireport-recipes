731b721ae4d6afd2a1dd9ec24698b650b97cd71a
Arjen van Bochoven <arjen@bochoven.net>
2015-07-21 08:55:25 +0200

    Fixed copyright

--- MunkiReportPackager.py
+++ MunkiReportPackager.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2010 Per Olofsson
+# Copyright 2015 Arjen van Bochoven
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
